c24836f1af67ab4f1047474ade69f6752d23f57a
bitstorm <bitstorm@tutanota.com>
2021-05-24 17:11:49 +0200

    initial commit to new git repo

---
+++ MAP4/MAP4.fs
@@ -0,0 +1,88 @@
+namespace MAP4.MAP4
+open System
+open System.Collections.Generic
+open GraphMolWrap
+open MAP4
+open MAP4.MHFP
+open NumpyDotNet
+open Utils
+
+type FP =
+    | Folded of ndarray
+    | Unfolded of ndarray
+
+type MAP4Calculator(?dimensions:int, ?radius:int, ?isCounted:bool, ?returnString:bool) =
+    member _.dimensions = defaultArg dimensions 1024
+    member _.radius = defaultArg radius 2
+    member _.isCounted = defaultArg isCounted false
+    member _.returnString = defaultArg returnString false
+    member this.encoder = MHFP(this.dimensions)
+    
+    member private this.getAtomEnvs(mol:RWMol) =
+        let ids = atomsSeq mol |> Seq.map (fun at -> int(at.getIdx()))
+        let envs = ids |> Seq.map (MHFP.singleAtomIdxEnvironmentShingle mol this.radius)
+        Seq.zip ids envs |> Map.ofSeq
+    
+    member private this.allPairs(mol:RWMol, atomEnvs:Map<int,Set<string>>) =
+        let atomPairs = List<string>()
+        let distanceMatrix = distanceMatrix mol
+        let numAtoms = int(mol.getNumAtoms())
+        let shingleDict=Dictionary<string,int>()
+        
+        for ids in (comb 2 [0..numAtoms-1]) do
+            let idx1 = ids.[0]
+            let idx2 = ids.[1]
+            let dist = int(distanceMatrix.[idx1, idx2]).ToString()
+
+            for i in [0..this.radius-1] do
+                let envA = (Set.toList atomEnvs.[idx1]).[i]
+                let envB = (Set.toList atomEnvs.[idx2]).[i]
+                let ordered = List.sort (envA :: envB :: [])
+                let mutable shingle = $"{ordered.[0]}|{dist}|{ordered.[1]}"
+                if this.isCounted then
+                    if not(shingleDict.ContainsKey(shingle)) then
+                        shingleDict.[shingle] <- 0
+                    shingleDict.[shingle] <- shingleDict.[shingle] + 1 
+                    shingle <- $"|{shingleDict.[shingle]}"
+                atomPairs.Add shingle
+        Set.toList(Set.ofArray(atomPairs.ToArray()))
+        
+    member private this.calculateInternal(mol:RWMol) =
+        this.allPairs(mol, this.getAtomEnvs(mol))
+        
+    member private this.fold(pairs) =
+        let fpHash = MHFP.hash pairs
+        Folded (MHFP.fold this.dimensions fpHash)
+        
+    member this.calculateFolded(mol) =
+        let atomEnvPairs = this.calculateInternal mol
+        this.fold atomEnvPairs
+
+    member this.calculate(mol) =
+        let shingles = this.calculateInternal mol
+        Unfolded (this.encoder.fromMolecularShingling shingles)
+        
+    member this.calculateMany(mols:RWMol list) =
+        Array.Parallel.map this.calculate (List.toArray mols)
+        
+    member this.calculateManyFolded(mols: RWMol list) =
+        Array.Parallel.map this.calculateFolded (List.toArray mols)
+        
+    member this.distance(fpA: FP, fpB: FP) =
+        let dist =
+            match fpA, fpB with
+            | Folded a, Folded b -> MHFP.distance a b
+            | Unfolded a, Unfolded b -> MHFP.distance a b
+            | _, _ -> raise(ArgumentException("both arguments must be folded or unfolded fingerprints"))
+        dist 
+
+        
+
+        
+    
+
+        
+        
+    
+
+        
---
+++ MAP4/Utils.fs
@@ -0,0 +1,109 @@
+namespace MAP4
+
+open System.Collections.Generic
+open System
+open System.Security.Cryptography
+open GraphMolWrap
+open NumpyDotNet
+open FSharp.Collections
+module Utils =
+    type Smiles = string
+    
+    let sha1 = HashAlgorithm.Create("SHA1")
+
+    let encoding = System.Text.UTF8Encoding()
+
+    let listToVec (l:int Set) =
+        let result = new Int_Vect()
+        l |> Set.iter result.Add
+        result
+        
+    let intToVec(i: int) =
+        let result = new Int_Vect()
+        result.Add(i)
+        result
+
+    /// Create a seq of all atoms from a molecule
+    let atomsSeq (mol:ROMol) =
+        seq{for atom in mol.getAtoms() do yield atom}
+
+    /// convert a string into a byte array using its utf-8 representation
+    let stringToBytes (s: string) =
+        encoding.GetBytes s
+        
+    /// Convert a byte array into a uint32    
+    let bytesToInt (b:byte[]) endIndex =
+        BitConverter.ToUInt32(b.[0..endIndex], 0)
+    /// Use the first N bytes of the sha1 hash of a string and interpret them as uint32    
+    let stringToHash (endAtByte:int) (s: string) =
+        let bytes = stringToBytes s
+        let hash = sha1.ComputeHash(bytes)
+        bytesToInt hash endAtByte
+          
+    let ivvToListList (ivv: Int_Vect_Vect) =
+        List.ofSeq(seq{ for i in ivv do List.ofSeq(seq{for j in i do j })})
+
+    let randomArray(size:int, rand: np.random, minVal:int64, maxVal: uint64) =
+        let h = HashSet()
+        while h.Count < size do
+            let v = rand.randint(minVal, Nullable(maxVal), newshape=shape(1), dtype=np.UInt32)
+            h.Add(uint32 v) |> ignore
+        let rand_list = Seq.fold (fun l se -> uint32(se)::l) [] h
+        let result = np.array(Array.ofList(rand_list), dtype=np.UInt32)
+        result.reshape([|size|])
+        
+    let smilesToMol(smiles: Smiles) (sanitize: bool) =
+        try
+            Some (RWMol.MolFromSmiles(smiles, 0, sanitize))
+        with
+        | e -> None
+        
+    /// Calculate the distance matrix for a molecule using the Floyd Warshall algorithm
+    /// This is a simpler version compared to the RDKit implementation always using the edge weight 1.0 
+    let distanceMatrix (mol:RWMol) =  // Floyd Warshall
+        let numAtoms = int(mol.getNumAtoms())
+        let dist = Array2D.create numAtoms numAtoms infinity
+        let numBonds = int(mol.getNumBonds())
+
+        for i in [0..numBonds-1] do
+            let bond = mol.getBondWithIdx(uint32(i))
+            let fst = int(bond.getBeginAtomIdx())
+            let snd = int(bond.getEndAtomIdx())
+            dist.[fst,snd] <- 1.0
+            dist.[snd,fst] <- 1.0
+            
+        let next = Array2D.create numAtoms numAtoms 0
+        
+        for k in [0..numBonds-1] do
+            for i in [0..numBonds-1] do
+                for j in [0..numBonds-1] do
+                    if dist.[i,k] + dist.[k,j] < dist.[i,j] then
+                        dist.[i,j] <- dist.[i,k] + dist.[k,j]
+                        next.[i,j] <- next.[i,k]
+        for i in [0..numAtoms-1] do
+            dist.[i,i] <- 0.0
+        dist       
+        
+    type MaybeBuilder() =
+        member _.Bind(m, f) =
+            Option.bind f m
+            
+        member _.Return(x) =
+            Some x
+            
+        member _.ReturnFrom(x) =
+            x
+            
+        member _.Zero() =
+            None
+            
+    let rec comb n l = 
+        match n, l with
+        | 0, _ -> [[]]
+        | _, [] -> []
+        | k, (x::xs) -> List.map ((@) [x]) (comb (k-1) xs) @ comb k xs
+
+    let maybe = MaybeBuilder()
+        
+            
+        
